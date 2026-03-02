c4c48d72adb8a571675d268f41ae613f317f9450
Adithi786 <123286437+Adithi786@users.noreply.github.com>
2026-03-01 20:53:49 -0500

    Day 1: first python script - variable and print statements

---
+++ day1.py
@@ -0,0 +1,19 @@
+#variables
+my_name = "Adithi Gundeti"
+my_role = "QA Engineer"
+years_experience = 1
+
+#print statements
+print("Hello, my name is ", my_name)
+print("My role is ", my_role)
+print("Years of experience: ", years_experience)
+
+
+#basic maths
+daily_study_minutes = 90
+weekly_study_minutes = daily_study_minutes * 5
+print("I study", daily_study_minutes, "minutes per day")
+print("I study", weekly_study_minutes,"minutes per week")
+
+#String formatting (modern way)
+print(f"In 6 months, {my_name} will be an automation engineer.")
